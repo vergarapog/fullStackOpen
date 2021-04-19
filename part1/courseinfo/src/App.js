9837708e6067db977a815127c2c43fb1638b97de
vergarapog <201801166@iacademy.edu.ph>
2021-04-19 13:49:38 +0800

    finished until part 1.5

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -48,29 +48,31 @@ const Total = (props) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   
 
   return (
     <div>
-      <h1><Header title={course} /></h1>
-      <Content parts={parts}/>
-      <Total parts={parts} />
+      <h1><Header title={course.name} /></h1>
+      <Content parts={course.parts}/>
+      <Total parts={course.parts} />
     </div>
   )
 }
